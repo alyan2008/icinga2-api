63d2caae1dc0ecf25ed5b72b007b7d59fe77c768
Nick <alyan2008@mail.ru>
2015-12-14 16:59:29 +0200

    v0.2

--- icinga2.py
+++ icinga2.py
@@ -2,6 +2,8 @@
 import argparse
 import sys
 import MySQLdb 
+import requests
+import os
 
 parser = argparse.ArgumentParser(description='Add server to Icinga2')
  
@@ -16,6 +18,10 @@ parser.add_argument('display_name',
 
 parser.add_argument('vars_service',
     choices=['service1', 'service2', 'service3'],
+    help="Service Name")
+
+parser.add_argument('vars_role',
+    choices=['role1', 'role2', 'role3'],
     help="Service Name")
  
 parser.add_argument('vars_production',
@@ -34,17 +40,24 @@ parser.add_argument('vars_type',
     choices=['type1', 'type2'],
     help="Type Name")
 
+parser.add_argument('vars_cpu',
+    choices=['cpu_load_0_20', 'cpu_load_0_30'],
+    help="CPU thresholds")
+
 args = parser.parse_args()
 
-conn = MySQLdb.connect(host= "localhost",
-                  user="root",
-                  passwd="Alyan2757622",
-                  db="icinga")
-x = conn.cursor()
-try:
-   x.execute("""INSERT INTO icinga_general (host, address, display_name, vars_service, vars_production, vars_datacenter, vars_app, vars_type) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""",(args.host, args.address, args.display_name, args.vars_service, args.vars_production, args.vars_datacenter, args.vars_app, args.vars_type))
-   conn.commit()
-except:
-   conn.rollback()
+#conn = MySQLdb.connect(host= "localhost",
+#                  user="root",
+#                  passwd="Alyan2757622",
+#                  db="icinga")
+#x = conn.cursor()
+#try:
+#   x.execute("""INSERT INTO icinga_general (host, address, display_name, vars_service, vars_production, vars_datacenter, vars_app, vars_type) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""",(args.host, args.address, args.display_name, args.vars_service, args.vars_production, args.vars_datacenter, args.vars_app, args.vars_type))
+#   conn.commit()
+#except:
+#   conn.rollback()
+#conn.close()
 
-conn.close()
+print(args.host)
+
+os.system('curl -k -s -u icinga:icinga -H \'Accept: application/json\' -X PUT \'https://192.168.253.134:5665/v1/objects/hosts/%s\' -d \'{ "templates": [ "generic-host" ], "attrs": { "address": "%s", "check_command": "hostalive", "display_name" : "%s", "vars.service" : "%s", "vars.production" : "%s", "vars.datacenter" : "%s", "vars.app" : "%s", "vars.type" : "%s", "vars.role" : "%s", "vars.cpu" : "%s"} }\'' % (args.host, args.address, args.display_name, args.vars_service, args.vars_production, args.vars_datacenter, args.vars_app, args.vars_type, args.vars_role, args.vars_cpu))
